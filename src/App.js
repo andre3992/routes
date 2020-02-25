d00ddefe460ab7d0e65d7b24a0d92ede4d6e6ef4
andre3992 <andre.regedor@hotmail.com>
2020-02-25 20:22:25 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
   Link
@@ -9,7 +8,6 @@ import {
 
 function App() {
   return (
-    <Router>
       <div>
         <nav>
           <ul>
@@ -43,7 +41,6 @@ function App() {
           </Route>
         </Switch>
       </div>
-    </Router>
   );
 }
 
